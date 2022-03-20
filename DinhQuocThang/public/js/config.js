35eddd71c39da63ab1527a146ccf93dd0cc68839
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-03-20 10:32:10 +0700

    Add files via upload

---
+++ DinhQuocThang/public/js/config.js
@@ -0,0 +1,33 @@
+const WEB = {
+  title: "PAYMENT",
+  favicon: "favicon.png",
+};
+
+const USER = {
+  username: "Đinh Mạnh Hoàng ",
+  blueTick: false,
+  avatar: "logonhan.png",
+  skills: [
+      "Social Media",
+      "Dịch Vụ Facebook"
+  ],
+  banks: [
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "60666620062007",
+          owner: "Dinh Quoc Thanh"
+      },
+      {
+         name: "Momo",
+         logo: "momo.jpg",
+         number: "0334403133",
+         owner: "Dinh Quoc Thanh",
+         fastTransfer: true
+    },
+
+
+
+
+  ]
+};
