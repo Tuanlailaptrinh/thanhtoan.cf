ca1a4e663a7a47f89a1bbdfe131ba6ccbb5adb60
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-03-27 09:11:21 -0700

    Add files via upload

---
+++ TranBaMan/public/js/config.js
@@ -0,0 +1,33 @@
+﻿const WEB = {
+  title: "PAYMENT",
+  favicon: "favicon.png",
+};
+
+const USER = {
+  username: "Trần Bá Mẫn ",
+  blueTick: false,
+  avatar: "avt.png",
+  skills: [
+      "Reseller",
+      "Dev"
+  ],
+  banks: [
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "055552006",
+          owner: "TRAN BA MAN"
+      },
+      {
+         name: "Momo",
+         logo: "momo.jpg",
+         number: "0325555020",
+         owner: "TRAN BA MAN",
+         fastTransfer: true
+    },
+
+
+
+
+  ]
+};
