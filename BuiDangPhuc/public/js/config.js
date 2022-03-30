f2d0c31348f7712926fd63c9f43e0cf3721bb8e0
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-03-30 12:44:21 +0700

    Add files via upload

---
+++ BuiDangPhuc/public/js/config.js
@@ -0,0 +1,51 @@
+const WEB = {
+  title: "PAYMENT",
+  favicon: "favicon.png",
+};
+
+const USER = {
+  username: "Bùi Đặng Phúc ",
+  blueTick: false,
+  avatar: "logoweb.png",
+  skills: [
+      "Hỗ trợ dịch vụ mạng xã hội",
+
+  ],
+  banks: [
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "0762313378",
+          owner: "BUI DANG PHUC"
+      },
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "1904567890",
+          owner: "BUI DANG PHUC"
+      },
+      {
+         name: "Momo",
+         logo: "momo.jpg",
+         number: "0762313367",
+         owner: "BUI DANG PHUC",
+         fastTransfer: true
+    },
+        {
+          name: "ZALO ",
+          logo: "zalo.png",
+          number: "0762313378",
+          owner: "BUI DANG PHUC"
+      },
+      {
+          name: "Website  ",
+          logo: "web.png",
+          number: "autosub19.com",
+          owner: "BUI DANG PHUC"
+      },
+
+
+
+
+  ]
+};
