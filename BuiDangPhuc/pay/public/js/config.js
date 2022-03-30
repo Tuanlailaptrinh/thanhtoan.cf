fd6b3269a1b2b88fa977d2e8a321511b4555d8b5
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-03-30 15:13:50 +0700

    Add files via upload

---
+++ BuiDangPhuc/pay/public/js/config.js
@@ -0,0 +1,40 @@
+const WEB = {
+  title: "PAYMENT",
+  favicon: "favicon.png",
+};
+
+const USER = {
+  username: "Bùi Đặng Phúc ",
+  blueTick: false,
+  avatar: "logoweb.png",
+  skills: [
+      "Hỗ trợ dịch vụ mạng xã hội",
+
+  ],
+  banks: [
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "0762313378",
+          owner: "BUI DANG PHUC"
+      },
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "1904567890",
+          owner: "BUI DANG PHUC"
+      },
+      {
+         name: "Momo",
+         logo: "momo.jpg",
+         number: "0762313367",
+         owner: "BUI DANG PHUC",
+         fastTransfer: true
+    },
+        
+
+
+
+
+  ]
+};
---
+++ BuiDangPhuc/public/js/config.js
@@ -0,0 +1,48 @@
+const __DATA__ = {
+    'config': {
+        'title': 'Home',
+        'favicon': 'home.png',
+        'notice': 'Hi Bạn Nhé !'
+    },
+    'cover-image': 'bia.gif',
+    'avatar-image': 'avatar.png',
+    'name': 'Bùi Đăng Phúc',
+    'is_tick': true,
+    'skills': ['Developer', 'Socila Media', ],
+    'bio-storys': [{
+        'image': 'admin.png',
+        'content': 'LIÊN HỆ ADMIN',
+        'author': 'Bùi Đăng Phúc',
+    }, ],
+    'links': [{
+        'image': 'wallet.png',
+        'title': 'Thông Tin Thanh Toán',
+        'show-value': 'Bùi Đăng Phúc',
+        'value': 'https://thanhtoan.cf/BuiDangPhuc/pay/',
+        'type': 0,
+    }, {
+        'image': 'facebook.jpg',
+        'title': 'Facebook',
+        'show-value': '@fucdepza19',
+        'value': 'https://www.facebook.com/fucdepza19',
+        'type': 0,
+    }, {
+        'image': 'zalo.jpg',
+        'title': 'Zalo',
+        'show-value': 'Bùi Đăng Phúc',
+        'value': 'https://zalo.me/0762313378 ',
+        'type': 0,
+    }, {
+        'image': 'phone.png',
+        'title': 'Website',
+        'show-value': 'Autosub19.com',
+        'value': 'https://autosub19.com/',
+        'type': 0,
+    }, {
+        'image': 'tele.jpg',
+        'title': 'Telegram',
+        'show-value': 'Bùi Đăng Phúc',
+        'value': 'https://t.me/iamphucfucdepza',
+        'type': 0,
+    }, ]
+};
