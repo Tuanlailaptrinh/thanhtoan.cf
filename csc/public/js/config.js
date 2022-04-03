c1dc2f4feba6e679b8004baa36b22e4b330e9e65
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-04-03 09:34:50 +0700

    Add files via upload

---
+++ csc/public/js/config.js
@@ -0,0 +1,30 @@
+const WEB = {
+  title: "PAYMENT",
+  favicon: "favicon.png",
+};
+
+const USER = {
+  username: "Nguyễn Duy Tình",
+  blueTick: false,
+  avatar: "logonhan.png",
+  skills: [
+      "It",
+      "Coder"
+  ],
+  banks: [
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "69999999992007",
+          owner: "NGUYEN DUY TINH"
+      },
+      {
+         name: "Momo",
+         logo: "momo.jpg",
+         number: "0379619546",
+         owner: "NGUYEN DUY TINH",
+         fastTransfer: true
+    },
+
+  ]
+};
