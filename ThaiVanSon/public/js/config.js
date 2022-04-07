5318e8f540c570ebe4cfff0558f47c2c17cf84b2
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-04-07 21:06:09 +0700

    Add files via upload

---
+++ ThaiVanSon/public/js/config.js
@@ -0,0 +1,31 @@
+const WEB = {
+  title: "PAYMENT",
+  favicon: "favicon.png",
+};
+
+const USER = {
+  username: "Thái Văn Sơn",
+  blueTick: false,
+  avatar: "logonhan.png",
+  skills: [
+      "Bán Acc Game",
+      "Dịch Vụ Facebook"
+  ],
+  banks: [
+      
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "88586861234",
+          owner: "THAI VAN SON"
+      },
+      {
+         name: "Momo",
+         logo: "momo.jpg",
+         number: "0338086040",
+         owner: "THAI VAN SON",
+         fastTransfer: true
+    },
+
+  ]
+};
